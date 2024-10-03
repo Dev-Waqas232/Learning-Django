2c637ba5957fb6885abda927153fcde79b92866b
Waqas Munir <devwaqas232@gmail.com>
2024-10-03 15:17:00 +0500

    Fetched Data from Database

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 
-HTML_STRING = """
-<h1>Hello World</h1>
-"""
+from blog.models import Blog
 
 
 def home_view(request):
+
+    blog_obj = Blog.objects.get(id=1)
+
+    blog_title = blog_obj.title
+    blog_content = blog_obj.content
+
+    H1_STRING = f"""
+    <h1>{blog_title}</h1>
+    """
+
+    P_STRING = f"""
+    <p>{blog_content}</p>
+    """
+
+    HTML_STRING = H1_STRING + P_STRING
+
     return HttpResponse(HTML_STRING)
