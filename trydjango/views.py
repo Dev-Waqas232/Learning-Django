81ed735c5f5744bd7fff7f3809257e887a527a2b
Waqas Munir <devwaqas232@gmail.com>
2024-10-04 11:43:43 +0500

    Worked with templates

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from blog.models import Blog
 
@@ -7,17 +8,7 @@ def home_view(request):
 
     blog_obj = Blog.objects.get(id=1)
 
-    blog_title = blog_obj.title
-    blog_content = blog_obj.content
-
-    H1_STRING = f"""
-    <h1>{blog_title}</h1>
-    """
-
-    P_STRING = f"""
-    <p>{blog_content}</p>
-    """
-
-    HTML_STRING = H1_STRING + P_STRING
-
-    return HttpResponse(HTML_STRING)
+    return render(request, "home.html", {
+        "title": blog_obj.title,
+        "content": blog_obj.content
+    })
