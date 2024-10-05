b8db3efb30b952545888cd220b3df69d260a2642
Waqas Munir <devwaqas232@gmail.com>
2024-10-05 17:01:44 +0500

    Customized Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Blog
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "content"]
+    search_fields = ["id"]
+
+
+admin.site.register(Blog, BlogAdmin)
